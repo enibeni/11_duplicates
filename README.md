5d8cc20b7ca954a0ed42fa75ed9b6a4f58da45bc
Sergey Morozov <sergeymorozov@MacBook-Pro-Sergey.local>
2018-01-02 19:01:32 +0300

    готов основной функционал

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Скрипт для поиска файлов дубликатов в указанной директории.
+
+# Как запустить
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5.
+В качестве параметра нужно указать директорию в которой нужно найти дубликаты.
+
+Пример выполнения скрипта, Python 3.5:
+
+```bash
+
+$ python3 duplicates.py dir_with_files/
+Сканирую...dir_with_files/
+Сканирую...dir_with_files/dir_2
+Сканирую...dir_with_files/dir_2/dir_2_1
+Сканирую...dir_with_files/dir_1
+Сканирую...dir_with_files/dir_1/dir_1_2
+Сканирую...dir_with_files/dir_1/dir_1_1
+Сканирую...dir_with_files/dir_1/dir_1_1/dir_1_1_1
+
+Найдены дубликаты:
+Названия могут быть разными,но содержание одинаковым
+___________________
+dir_with_files/dir_1/file.txt
+dir_with_files/dir_1/dir_1_2/filed.txt
+dir_with_files/dir_1/dir_1_1/file.txt
+dir_with_files/dir_1/dir_1_1/dir_1_1_1/file.txt
+
+
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,59 @@
+import os
+import sys
+import hashlib
 
-if __name__ == '__main__':
-    pass
+
+def get_duplicate_files(start_path):
+    duplicate_files = {}
+    for dir_name, subdir_list, file_list in os.walk(start_path):
+        print("Сканирую...{}".format(dir_name))
+        for filename in file_list:
+            path_to_file = os.path.join(
+                dir_name,
+                filename
+            )
+            file_hash = hashfile(path_to_file)
+            if file_hash in duplicate_files:
+                duplicate_files[file_hash].append(path_to_file)
+            else:
+                duplicate_files[file_hash] = [path_to_file]
+    return duplicate_files
+
+
+def hashfile(path, blocksize=65536):
+    hasher = hashlib.md5()
+    afile = open(path, 'rb')
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+    return hasher.hexdigest()
+
+
+def print_duplicate_files(duplicate_files):
+    results = list(filter(lambda x: len(x) > 1,
+                          duplicate_files.values()))
+    if len(results) > 0:
+        print('\nНайдены дубликаты:')
+        print('Названия могут быть разными,но содержание одинаковым')
+        print('___________________')
+        for result in results:
+            for subresult in result:
+                print(subresult)
+            print('___________________')
+    else:
+        print('Дубликатов не найдено')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        start_path = sys.argv[1]
+        if os.path.exists(start_path):
+            duplicate_files = get_duplicate_files(start_path)
+            print_duplicate_files(duplicate_files)
+        else:
+            print("Введено неверное имя директории")
+    else:
+        print("Для работы программы введите имя директории")
+
